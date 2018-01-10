12d33412b5b57a05a5e9095c6f1723aa046c7c2d
Derbes Utebaliyev <uderbes@gmail.com>
2018-01-10 09:57:51 +0600

    first upload

---
+++ lab1/First Upload/First Upload/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace First_Upload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
