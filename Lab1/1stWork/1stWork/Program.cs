f3797d0afaadaf050860160a337961761d7687ea
Derbes Utebaliyev <uderbes@gmail.com>
2018-01-11 18:17:43 +0600

    1stwork

---
+++ Lab1/1stWork/1stWork/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1stWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string[] arr = line.Split(' ');
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = int.Parse(arr[i]);
+                if (sum == 1) Console.WriteLine("NO");
+                else
+                {
+                    int e = 0;
+                    for (int j = 2; j <= sum; j++)
+                    {
+                        if (sum % j == 0) e++;
+                    }
+                    if (e == 1 || sum % 2 != 0) Console.WriteLine("NO");
+                    else Console.WriteLine("YES");
+                }
+            }
+
+            Console.ReadKey();          
+        }
+    }
+    
+}
